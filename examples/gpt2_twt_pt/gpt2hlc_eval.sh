6799d04cf21ffc21508012c399a967439430e3a0
Nikita Soni <nisoni@cs.stonybrook.edu>
2024-06-16 16:42:29 -0400

    Update gpt2hlc_eval.sh

--- examples/gpt2_twt_pt/gpt2hlc_eval.sh
+++ examples/gpt2_twt_pt/gpt2hlc_eval.sh
@@ -1,8 +1,7 @@
-CUDA_VISIBLE_DEVICES=2 \
-python /chronos_data/nisoni/NAACL_Tutorial/HaRT/run_continue_pt_gpt_twt.py \
-    --model_name_or_path /chronos_data/nisoni/analysing_hart_user_states/GPT2_TWT_PT_3June2024/outputs/gpt2_twt_pt_60bs \
+python HaRT/run_continue_pt_gpt_twt.py \
+    --model_name_or_path hlab/hart-gpt2sml-twt-v1 \
     --do_eval \
-    --output_dir /chronos_data/nisoni/NAACL_Tutorial/outputs/gpt2_twt_wassa_eval \
-    --per_device_eval_batch_size 60 \
+    --output_dir outputs/gpt2_twt_wassa_eval \
+    --per_device_eval_batch_size 30 \
     --block_size 200 \
-    --validation_file /chronos_data/nisoni/NAACL_Tutorial/data/essay_train_table_5_essays.pkl \
+    --validation_file $1 \
