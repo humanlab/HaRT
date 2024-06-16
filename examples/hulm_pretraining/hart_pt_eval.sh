48b8c7a7c35c1992774ad66dadbe1d8d56286944
Nikita Soni <nisoni@cs.stonybrook.edu>
2024-06-16 16:44:28 -0400

    Update hart_pt_eval.sh

--- examples/hulm_pretraining/hart_pt_eval.sh
+++ examples/hulm_pretraining/hart_pt_eval.sh
@@ -1,15 +1,14 @@
-CUDA_VISIBLE_DEVICES=2 \
 python -O HaRT/run_continue_pt_hart.py \
-    --model_name_or_path /chronos_data/nisoni/HaRT_Twt_model \
+    --model_name_or_path hlab/hart-gpt2sml-twt-v1 \
     --add_history \
     --initial_history HaRT/initial_history/initialized_history_tensor.pt \
     --extract_layer 11 \
     --do_eval \
-    --output_dir /chronos_data/nisoni/NAACL_Tutorial/outputs/hart_wassa_eval \
+    --output_dir outputs/hart_wassa_eval \
     --per_device_eval_batch_size 15 \
     --block_size 1024 \
-    --validation_file /chronos_data/nisoni/NAACL_Tutorial/data/essay_train_table_5_essays.pkl \
-    # --overwrite_output_dir \
+    --validation_file $1 \
+    --overwrite_output_dir \
 
     
     
