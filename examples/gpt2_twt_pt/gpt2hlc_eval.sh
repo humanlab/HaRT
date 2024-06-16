8653de4bcb28713876c55a86e8256fb57c320436
Nikita Soni <nisoni@cs.stonybrook.edu>
2024-06-16 16:45:58 -0400

    Update gpt2hlc_eval.sh

--- examples/gpt2_twt_pt/gpt2hlc_eval.sh
+++ examples/gpt2_twt_pt/gpt2hlc_eval.sh
@@ -1,7 +1,7 @@
 python HaRT/run_continue_pt_gpt_twt.py \
-    --model_name_or_path hlab/hart-gpt2sml-twt-v1 \
+    --model_name_or_path hlab/gpt2sml-hlc-twt-v1 \
     --do_eval \
     --output_dir outputs/gpt2_twt_wassa_eval \
-    --per_device_eval_batch_size 30 \
+    --per_device_eval_batch_size 60 \
     --block_size 200 \
     --validation_file $1 \
